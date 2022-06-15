9e8c24527b08029c2c29550af7d1522d324f6e50
Max Jakobitsch <minigugler@gmail.com>
2022-06-15 23:50:32 +0200

    feat: share code in android

--- lib/view_code.dart
+++ lib/view_code.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:epc_qr/qr_data.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ViewCodePage extends StatelessWidget {
   ViewCodePage({Key? key, required this.qrData}) : super(key: key);
@@ -19,12 +25,33 @@ class ViewCodePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Payment Information'),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () {
-              final img = QrPainter.withQr(qr: qrCode).toImageData(200);
-            },
-          ),
+          Builder(builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: () async {
+                final ByteData? img =
+                    await QrPainter.withQr(qr: qrCode).toImageData(200);
+                if (img == null) return;
+                final List<int> imgData = img.buffer
+                    .asUint8List(img.offsetInBytes, img.lengthInBytes);
+
+                if (kIsWeb || !(Platform.isAndroid || Platform.isIOS)) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                          'Saving is not yet supported for this platform.'),
+                    ),
+                  );
+                } else {
+                  final dir = await getTemporaryDirectory();
+                  final file = File('$dir/payment-info.png');
+                  await file.writeAsBytes(imgData);
+
+                  await Share.shareFiles([file.path], mimeTypes: ['image/png']);
+                }
+              },
+            );
+          }),
         ],
       ),
       body: ListView(
