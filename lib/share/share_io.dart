bb80155ac0617b2b5ac5f14ddfc50a9f63b94ddb
Max Jakobitsch <minigugler@gmail.com>
2022-07-21 21:27:46 +0200

    fix: share on mobile

--- lib/share/share_io.dart
+++ lib/share/share_io.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 // see https://pub.dev/packages/share_plus#platform-support
-bool get shareFeatureAvailable => !(Platform.isWindows || !Platform.isLinux);
+bool get shareFeatureAvailable => !(Platform.isWindows || Platform.isLinux);
 
 Future<void> shareFile(String name, List<int> data, {String? mimeType}) async {
   final dir = await getTemporaryDirectory();
-  final file = File('$dir/$name');
+  final file = File(p.join(dir.path, name));
   await file.writeAsBytes(data);
 
   List<String>? mimeTypes;
