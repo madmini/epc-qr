e2f9d23ca04351c7e1d9d3a65013a520cc7d6be1
Max Jakobitsch <minigugler@gmail.com>
2022-07-23 20:34:34 +0200

    feat: add infobox popup

---
+++ lib/widgets/about_button.dart
@@ -0,0 +1,75 @@
+import 'package:epc_qr/widgets/linkify_on_open.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+
+const kGitUrl = String.fromEnvironment('GIT_URL');
+const kGitBranch = String.fromEnvironment('GIT_BRANCH');
+const kCommitHash = String.fromEnvironment('COMMIT_HASH');
+const kCiProvider = String.fromEnvironment('CI_PROVIDER');
+const kVersionTag = String.fromEnvironment('VERSION_TAG');
+
+class AboutButton extends StatelessWidget {
+  const AboutButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.info),
+      onPressed: () => showAboutDialog(
+        context: context,
+        applicationName: 'EPC-QR Generator',
+        applicationVersion: kVersionTag != '' ? kVersionTag : null,
+        children: kGitUrl != '' ? const [AboutSourceText()] : null,
+      ),
+    );
+  }
+}
+
+class AboutSourceText extends StatelessWidget {
+  const AboutSourceText({Key? key}) : super(key: key);
+
+  TextStyle _codeStyle(BuildContext context) => TextStyle(
+        fontFamily: 'RobotoMono',
+        color: Theme.of(context).textTheme.headline4?.color,
+        fontWeight: Theme.of(context).brightness == Brightness.light
+            ? FontWeight.bold
+            : null,
+      );
+
+  TextStyle _linkStyle(BuildContext context) => TextStyle(
+        fontFamily: 'RobotoMono',
+        color: Theme.of(context).brightness == Brightness.dark
+            ? const Color(0xFF9BCAFF)
+            : const Color(0xFF0062A0),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    if (kGitUrl == '') return const SizedBox();
+
+    return SelectableText.rich(
+      TextSpan(children: [
+        const TextSpan(
+          text: 'Built${kCiProvider != '' ? ' by $kCiProvider' : ''} from ',
+        ),
+        LinkifySpan(
+          text: kGitUrl,
+          linkStyle: _linkStyle(context),
+          onOpen: onOpenLaunchUrl,
+        ),
+        if (kGitBranch != '')
+          TextSpan(children: [
+            const TextSpan(text: ' (in '),
+            TextSpan(text: kGitBranch, style: _codeStyle(context)),
+            const TextSpan(text: ')'),
+          ]),
+        if (kCommitHash != '')
+          TextSpan(children: [
+            const TextSpan(text: ' at '),
+            TextSpan(text: kCommitHash, style: _codeStyle(context)),
+            const TextSpan(text: '.'),
+          ]),
+      ]),
+    );
+  }
+}
